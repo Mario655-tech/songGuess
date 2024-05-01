fc59b1d498899ba4b1054228e32295153282a2a1
Mario655-tech <mariobatinic10@gmail.com>
2024-05-01 09:43:45 +0200

    Added artists.

--- artistsJS.js
+++ artistsJS.js
@@ -37,6 +37,48 @@ checkboxes.forEach(function(checkbox) {
         case "8":
             links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=51914");
             break;
+        case "9":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=90795");
+            break;            
+        case "10":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=456144");
+            break;           
+        case "11":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=116238");
+            break;   
+        case "12":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=179570");
+            break;   
+        case "13":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=9");
+            break;   
+        case "14":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=70");
+            break; 
+        case "15":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=2");
+            break;  
+        case "16":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=21");
+            break;  
+        case "17":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=94");
+            break; 
+        case "18":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=148736");
+            break;  
+        case "19":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=113873");
+            break;
+        case "20":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=98818");
+            break;
+        case "21":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=61557");
+            break;
+        case "22":
+            links.add("https://songsexcerpt.mohd.app/api/v1/getRandomExcerpt?artists=118271");
+            break;                      
       }
     } else {
       // If the checkbox is unchecked, remove its value from links set
